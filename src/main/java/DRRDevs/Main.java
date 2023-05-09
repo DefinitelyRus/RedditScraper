129616201747c86f1d7991a7d343ea4ae341a735
christiandarvs <christiandarvin4@gmail.com>
2023-05-09 15:26:36 +0800

    Update Main.java

--- src/main/java/DRRDevs/Main.java
+++ src/main/java/DRRDevs/Main.java
@@ -18,10 +18,9 @@ package DRRDevs;
 	- Otherwise, player 2 wins.
  */
 
+import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
-		// yow red
-		System.out.println("Hello world!");
-		//bruh darvs titex
+		Scanner input = new Scanner(System.in);
 	}
 }
