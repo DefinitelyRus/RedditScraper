2d28c5eb7e5f9c678d3fabb7db81d5940ef2fc08
christiandarvs <christiandarvin4@gmail.com>
2023-05-09 15:31:14 +0800

    Update Main.java

--- src/main/java/DRRDevs/Main.java
+++ src/main/java/DRRDevs/Main.java
@@ -22,6 +22,8 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
+		System.out.println("Do you want to start? ");
+		String prompt = input.next();
+
 	}
 }
